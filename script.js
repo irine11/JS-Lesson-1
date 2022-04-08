55b9fd68e1c3dd6edc9d8455302f3d88f6159da6
irine11 <96834447+irine11@users.noreply.github.com>
2022-04-09 01:51:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 //7 exercise
-let array = [
-    [2, -3, 5, 10],
-    [25, -24, -11, 100],
-    [-6, -7, 10]
-]
+let array = [[2, -3, 5, 10], [25, -24, -11, 100], [-6, -7, 10]]
+
 for (let i = 0; i < array.length; i++) {
     let numbers = array[i];
     for (let x = 0; x < numbers.length; x++) {
